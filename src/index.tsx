faef01ee2b9a51aea248f4a81129669aa86718b0
Habibullo <hhalimjonov1997@gmail.com>
2024-07-05 13:49:40 +0500

    feat: MUI standard integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import ReactDOM  from 'react-dom';
-// import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import './app/css/index.css'
-
-// const container = document.getElementById('root')!;
-// const root = createRoot(container);
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import './app/css/index.css';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
