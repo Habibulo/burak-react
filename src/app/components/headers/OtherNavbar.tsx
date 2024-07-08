d71108fea690b757ca3b0b502ba3facc33681429
Habibullo <hhalimjonov1997@gmail.com>
2024-07-08 22:46:45 +0500

    develop otherNavbar component

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,3 +1,44 @@
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function OtherNavbar(){
-    return <div>OtherNavbar</div>
+    const authMember = null
+    return (
+        <div className="other-navbar">
+        <Container className="navbar-container">
+            <Stack className="menu">
+                <Box>
+                    <NavLink to={'/'} activeClassName="underline">
+                        <img src="/icons/burak.svg" className="brand-logo"/>
+                    </NavLink>
+                </Box>
+                <Stack  className="links">
+                <Box className={"hover-line"}> <NavLink to={'/'}>Home</NavLink></Box>
+                <Box className={"hover-line"}> <NavLink to={'/products'} activeClassName="underline">Produts</NavLink></Box>
+                <Box className={"hover-line"}> <NavLink to={'/help'} activeClassName="underline">Help</NavLink></Box>
+                <Box>
+                    {authMember ? (<Box className={"hover-line"}><NavLink to={'/orders'} activeClassName="underline">Orders</NavLink></Box>)
+                    : null }
+                </Box>
+                <Box>
+                    { authMember? (<Box className={"hover-line"}> <NavLink to={'/member-page'} activeClassName="underline">MyPage</NavLink></Box>) 
+                    : null}
+                </Box>
+                    {/* Basket */}
+                {!authMember ? 
+                    (<Box>
+                        <Button variant="contained" className="login-button">Login
+                        </Button>
+                    </Box>) : ( 
+                    <img 
+                        className="user-avatar"
+                        src="/icons/default-user.svg"
+                        aria-haspopup ={'true'}
+                    />
+                )}
+                </Stack>
+            </Stack>
+        </Container>
+    </div>
+    )
 }
