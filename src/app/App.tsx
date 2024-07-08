911e92c2eb765b0436c4cb3246ac3ecf57c17f15
Habibullo <hhalimjonov1997@gmail.com>
2024-07-08 17:01:15 +0500

    feat: Header&Navbar developed in React

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './css/app.css';
 import {Link, Route, Switch, useLocation } from 'react-router-dom';
 // import { RippleBadge } from './MaterialTheme/styled';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
@@ -14,32 +13,24 @@ import path from 'path';
 import { HomeNavbar } from './components/headers/HomeNavbar';
 import { OtherNavbar } from './components/headers/OtherNavbar';
 import { Footer } from './components/footer/footer';
-
+import './css/app.css';
+import './css/navbar.css'
+import { HelpPage } from './Screens/helpPage';
 function App() {
   const location  = useLocation()
   console.log("console:" , location);
-  
   return (
     <>
       {location.pathname === '/' ? <HomeNavbar/> : <OtherNavbar/> }
-      {/* A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
       <Switch>
-        <Route path="/products">
-          <ProductsPage />
-        </Route>
-        <Route path="/orders">
-          <OrdersPage />
-        </Route>
-        <Route path="/member-page">
-          <UserPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
+        <Route path="/"><HomePage/> </Route>
+        <Route path="/products"><ProductsPage /></Route>
+        <Route path="/orders"><OrdersPage /></Route>
+        <Route path="/member-page"><UserPage/></Route>
+        <Route path="/help"><HelpPage /></Route>
       </Switch>
       <Footer/>
-      </>
+    </>
   );
 }
 
--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,3 +1,43 @@
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function HomeNavbar(){
-    return <div>HomeNavbar</div>
+    const authMember = null
+
+    return <div className="home-navbar">
+        <Container sx={{mt:"55px", height: "642px"}}>
+            <Stack 
+                sx={{height: "50px"}} 
+                flexDirection={"row"}
+                justifyContent={"space-between"}
+                alignItems={"center"}>
+                <Box>
+                    <NavLink to={'/'} activeClassName="underline">
+                        <img src="/icons/burak.svg" style={{width: '125px', height: '30px'}} alt="Burak"/>
+                    </NavLink>
+                </Box>
+                <Stack 
+                    minWidth={"700px"}
+                    flexDirection={"row"}
+                    justifyContent={"space-between"}
+                    alignItems={"center"}
+                    >
+                </Stack>
+                <Box className={"hover-line"}> <NavLink to={'/'} activeClassName="underline">Home</NavLink></Box>
+                <Box className={"hover-line"}> <NavLink to={'/products'} activeClassName="underline">Produts</NavLink></Box>
+                <Box className={"hover-line"}> <NavLink to={'/help'} activeClassName="underline">Help</NavLink></Box>
+                <Box>
+                    {authMember ? (<Box className={"hover-line"}><NavLink to={'/orders'} activeClassName="underline">Orders</NavLink></Box>)
+                    : null }
+                </Box>
+                <Box>
+                    { authMember? (<Box className={"hover-line"}> <NavLink to={'/member-page'} activeClassName="underline">MyPage</NavLink></Box>) 
+                    : null}
+                </Box>
+                {/* Basket */}
+                {!authMember? (<Box><Button variant="contained" sx={{background: "#3776CC", color: 'f8f8ff'}} >Login</Button></Box>) : <img/>}
+            </Stack>
+            <Stack>DETAILS</Stack>
+        </Container>
+    </div>
 }
