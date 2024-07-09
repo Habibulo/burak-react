e6040a50790f9ef4331ef1005d8662760fb4620d
Habibullo <hhalimjonov1997@gmail.com>
2024-07-09 16:55:15 +0500

    homePage related components

--- document.ts
+++ document.ts
@@ -1,8 +1,22 @@
 /*
-DARSIMIZ REJASI
-Header va footerlarning ahamiyati
-Figma loyihasiga garab publishing buyicha
-rejalarni amalga oshiramiz
-Header va navbarni publishingnini amalga
-oshiramiz
+    DARSIMIZ REJASI
+        Header va footerlarning ahamiyati
+        Figma loyihasiga garab publishing buyicha
+        rejalarni amalga oshiramiz
+        Header va navbarni publishingnini amalga
+        oshiramiz
+*/
+
+
+/*
+    62_63_
+    DARSIMIZ REJASI
+    62_
+        - HomePage Screen Componentiga tegishli bo'lgan
+          Sectional Componentlarni hosil qilamiz
+        - Statistics Sectional Componentini develop gilamiz
+        - Mashxur taomlar ya'ni PopularDishes Sectional
+          Componentini develop qilamiz
+    63_
+
 */
---
+++ src/app/Screens/homePage/PopularDishes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function PopularDishes() {
+    return (
+        <div>
+            <Container>
+                Popular Dishes
+            </Container>
+        </div>
+    )
+}
