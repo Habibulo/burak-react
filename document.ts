fa43aec84b99e5775e4906828b8a0a1b22504367
Habibullo <hhalimjonov1997@gmail.com>
2024-07-12 11:04:05 +0500

    feat: develop homePage component

--- document.ts
+++ document.ts
@@ -10,13 +10,17 @@
 
 /*
     62_63_
-    DARSIMIZ REJASI
-    62_
-        - HomePage Screen Componentiga tegishli bo'lgan
-          Sectional Componentlarni hosil qilamiz
-        - Statistics Sectional Componentini develop gilamiz
-        - Mashxur taomlar ya'ni PopularDishes Sectional
-          Componentini develop qilamiz
+    DARSIMIZ REJASI    
+      - HomePage Screen Componentiga tegishli bo'lgan
+        Sectional Componentlarni hosil qilamiz
+      - Statistics Sectional Componentini develop gilamiz
+      - Mashxur taomlar ya'ni PopularDishes Sectional
+        Componentini develop qilamiz
     63_
-
+    DARSIMIZ REJASI
+      - Target Play haqida gaplashamiz
+      - Bosh sahifamizning NewDishes Screen Componentini hosil gilamiz
+      - Advertisement Sectional Componentini hosil qilamiz
+      - ActiveUsers Sectional Componentini hosil gilamiz
+      - HomePage Events Sectional Componentini hosil qilamiz
 */
